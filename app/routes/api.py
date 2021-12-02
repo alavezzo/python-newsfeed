2c031248203b153c329d19ab12e2564480f672b4
bowdenshot <anthonyelavezzo@gmail.com>
2021-12-01 21:07:47 -0800

    integrated session functionality and imported session data into home route

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, session
 from app.models import User 
 from app.db import get_db
+import sys 
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
@@ -9,15 +10,31 @@ def signup():
     data = request.get_json()
     db = get_db()
     
-    newUser = User(
-        username = data['username'],
-        email = data['email'],
-        password = data['password']
-    )
+    try: 
+        # attempt creating a new User
+        newUser = User(
+            username = data['username'],
+            email = data['email'],
+            password = data['password']
+        )
 
-    # save in database
+        db.add(newUser)
+        db.commit()
+        # if db.commit() fails the connection will remain in a pending state (won't notice in a local development setting but will lock up a production setting)
+       
+    except: 
+        # insert failed, so send error to front end
+        print(sys.exc_info()[0])
 
-    db.add(newUser)
-    db.commit()
+        #insert failed, so rollback and send error to front end and make it so connection doesn't stay in a pending state
+        db.rollback()
+        return jsonify(message = 'Signup failed'), 500
+        
+    # after login need a way to keep track of a user's logged in status, in express you could register the express-session npm package as middleware which exposes a req.session property on every request. In flask sessions belongf to a global object similar to g and request
+    # you can create sessions in Flask only if you've defined a secret key, code in app/_init_.py does that 
+
+    session.clear()
+    session['user_id'] = newUser.id
+    session['loggedIn'] = True
 
     return jsonify(id = newUser.id)
